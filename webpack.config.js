b96a7eefaec91788d8fef86d737a0e9844f62eb1
Guilhermecheng <62719629+Guilhermecheng@users.noreply.github.com>
2021-09-25 01:27:13 -0300

    commit 2

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,13 @@ module.exports = {
     mode: isDevelopment ? 'development' : 'production',
     // devtool is configuring a source map, for better evaluation of errors
     devtool: isDevelopment ? 'eval-source-map' : 'source-map', 
-    entry : path.resolve(__dirname, 'src', 'index.jsx'),
+    entry : path.resolve(__dirname, 'src', 'index.tsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename:'bundle.js'
     },
     resolve: {
-        extensions: ['.js', '.jsx'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
     },
     devServer: {
         // contentBase was changed to static
@@ -30,7 +30,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.jsx$/,
+                test: /\.(j|t)sx$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
